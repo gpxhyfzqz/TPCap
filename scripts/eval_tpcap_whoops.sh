07112b8163c08084296ed44bcc2d84bd481187cb
gpxhyfzqz <130033118+gpxhyfzqz@users.noreply.github.com>
2025-12-30 15:52:56 +0800

    Update eval_tpcap_whoops.sh

--- scripts/eval_tpcap_whoops.sh
+++ scripts/eval_tpcap_whoops.sh
@@ -3,21 +3,21 @@ cd $SHELL_FOLDER/..
 
 EXP_NAME=$1
 DEVICE=$2
-FLICKR_OUT_PATH=results/$EXP_NAME
+WHOOPS_OUT_PATH=results/$EXP_NAME
 
 TIME_START=$(date "+%Y-%m-%d-%H-%M-%S")
 LOG_FOLDER=logs/${EXP_NAME}_EVAL
 mkdir -p $LOG_FOLDER
 
-NOCAPS_LOG_FILE="$LOG_FOLDER/NOCAPS_${TIME_START}.log"
+WHOOPS_LOG_FILE="$LOG_FOLDER/WHOOPS_${TIME_START}.log"
 
 python -u eval_tpcap.py \
 --device cuda:$DEVICE \
 --name_of_datasets whoops \
---out_path=$FLICKR_OUT_PATH \
-|& tee -a  ${NOCAPS_LOG_FILE}
+--out_path=$WHOOPS_OUT_PATH \
+|& tee -a  ${WHOOPS_LOG_FILE}
 
 
 echo "==========================Whoops EVAL================================"
-python evaluation/cocoeval.py --result_file_path $FLICKR_OUT_PATH/whoops*.json |& tee -a  ${FLICKR_LOG_FILE}
+python evaluation/cocoeval.py --result_file_path $WHOOPS_OUT_PATH/whoops*.json |& tee -a  ${WHOOPS_LOG_FILE}
 
